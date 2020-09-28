5ddfbaf9a304f1872c6f4817b7f7ed48098c569c
Alessandro <sandromuniz27@hotmail.com>
2020-09-28 09:25:27 -0400

    feat: add routes auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,18 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const authControl = require('../controller/auth');
 
 router.get('/', (req, res) => {
     //carregar pagina login ou registro
 });
-router.get('/registro', (req, res) => {
+router.get('/registro', (_, res) => {
     res.render('../views/layouts/auth/registry');
 });
 router.post('/registro', authControl.createAccount);
-router.get('/login', (req, res) => {
+router.get('/login', (_, res) => {
     res.render('../views/layouts/auth/login');
 });
-router.post("/login", authControl.login);
-router.post("/login-side", authControl.login_side);
-router.get("/logout", authControl.logout);
+router.post('/login', authControl.login);
+router.post('/login-side', authControl.login_side);
+router.get('/logout', authControl.logout);
 
 module.exports = router;
