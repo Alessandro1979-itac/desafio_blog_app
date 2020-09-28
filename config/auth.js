d3b2428218439175da92925673e9fe6edca00879
Alessandro <sandromuniz27@hotmail.com>
2020-09-28 09:29:49 -0400

    feat: add config auth

--- config/auth.js
+++ config/auth.js
@@ -1,24 +1,24 @@
-require("../models/User")
-const localStrategy = require("passport-local").Strategy;
-const mongoose = require("mongoose");
+require('../models/User');
+const localStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
 const bcrypt = require("bcryptjs");
-const User = mongoose.model("users");
+const User = mongoose.model('users');
 
 module.exports = function (passport) {
-    passport.use(new localStrategy({ usernameField: 'email', passwordField: "password" }, (email, password, done) => {
+    passport.use(new localStrategy({ usernameField: 'email', passwordField: 'password' }, (email, password, done) => {
         User.findOne({ email: email }).then((user) => {
             if (!user) {
-                return done(null, false, { message: "Esta conta não existe" })
+                return done(null, false, { message: 'Esta conta não existe' });
             }
             if (user.eBlocked) {
-                return done(null, false, { message: "Você está bloqueado" })
+                return done(null, false, { message: 'Você está bloqueado' });
             }
 
             bcrypt.compare(password, user.password, (erro, batem) => {
                 if (batem) {
-                    return done(null, user)
+                    return done(null, user);
                 } else {
-                    return done(null, false, { message: "Senha incorreta" })
+                    return done(null, false, { message: 'Senha incorreta' });
                 }
             });
         });
