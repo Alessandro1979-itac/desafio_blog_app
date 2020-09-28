8150d8c44dc651ef070d5d5cfa8df44ef7ec1663
Alessandro <sandromuniz27@hotmail.com>
2020-09-27 22:47:20 -0400

    feat: add config db

---
+++ config/db.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        //mongoURI: "[. . .]"
+    }
+} else {
+    module.exports = { mongoURI: "mongodb://localhost/blogapp" }
+}
